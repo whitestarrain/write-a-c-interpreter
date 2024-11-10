8542cb52188cce2c4cc1fe6cd49bdd494e7a3fa2
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2024-11-11 00:25:50 +0800

    update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int fibonacci(int i) {
+    if (i <= 1) {
+        return 1;
+    }
+    return fibonacci(i-1) + fibonacci(i-2);
+}
+
+int main()
 {
-    printf("hello world");
+    int i;
+    i = 0;
+    while (i <= 10) {
+        printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
+        i = i + 1;
+    }
     return 0;
 }
